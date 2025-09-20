b14aab5eab87767613af57597d404d863bd5d3d2
aminayes <amina.yes03@gmail.com>
2025-09-20 20:34:20 +0500

    news js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
-const BASE_URL = "https://webfinalapi.mobydev.kz"
+const BASE_URL = "https://webfinalapi.mobydev.kz";
 
-async function name(params) {
-    
+async function fetchAndRenderNews() {
+    try {
+        const response = await fetch(`${BASE_URL}/news`);
+        if (!response.ok) throw new Error(`Ошибка HTTP: ${response.status}`);
+
+        const newsArray = await response.json();
+        document.querySelector('.news-grid').innerHTML = newsArray.map(news => `
+            // здесь будет код карточки
+            `).join('');
+    } catch (error) {
+        console.error('Ошибка при получении новостей:', error);
+    }
 }
+
+document.addEventListener('DOMContentLoaded', fetchAndRenderNews);
